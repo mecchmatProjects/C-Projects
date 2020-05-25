4f8a2b643136a680968d5204121f4275d1ecc0b8
Evhen Dyrda <geka.skype@gmail.com>
2020-05-25 16:30:49 +0300

    Create DataComparison.h

---
+++ Dyrda_2course_statistic_Project_DataComparison/DataComparison_c/DataComparison.h
@@ -0,0 +1,21 @@
+#include < stdio.h >  
+#include < stdlib.h >
+
+
+int count_c()
+{
+    int c = 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+          if(i == j){
+            if(a[i] == b[j]) {
+                ++ c;
+                break;
+            }
+            else {
+                printf(“Difference %d between array elements:  : %d\n”, i,abs(a[i]-b[j]));
+        }
+      }
+    }
+    return c;
+}

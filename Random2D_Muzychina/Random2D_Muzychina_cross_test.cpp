ee772ef0617d9eff114960cf9cbca2198a0df2bd
Anna Muzychina <70022930+AnnaMuza@users.noreply.github.com>
2020-12-10 21:23:50 +0200

    Add files via upload

---
+++ Random2D_Muzychina/Random2D_Muzychina_cross_test.cpp
@@ -0,0 +1,6 @@
+/* File Random2D_Muzychina_cross_test.cpp
+Realization of cross test for Random2D (32) (C++)
+Done by Anna Muzychina (group comp. mat. 2)
+Date 09.12.2020 */
+
+// Поскольку работа проекта основана на генераторах рандомных чисел, то результат работы файлов на С и С++ всегда будет разный

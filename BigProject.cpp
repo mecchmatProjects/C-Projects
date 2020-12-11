9d562a4bbafe5be6674f642f32df2a98b052fc3e
KutuzovaKate <75337110+KutuzovaKate@users.noreply.github.com>
2020-12-11 18:43:01 +0200

    Add files via upload

---
+++ BigProject.cpp
@@ -0,0 +1,15 @@
+﻿#include "ExponentialDistribution.h"
+#include "NormalDistribution.h"
+#include "StudentDistribution.h"
+#include "FisherDistribution.h"
+#include "PoissonDistribution.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << PoisonDistribution() << std::endl;
+    std::cout << FisherDistribution() << std::endl;
+    std::cout << NormalDistribution() << std::endl;
+    std::cout << StudentDistribution() << std::endl;
+    std::cout << ExponentialDistribution() << std::endl;
+}
